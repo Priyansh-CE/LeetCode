ace8c2a3fcf49539cd2bb4686a909075176eab92
Patel Priyansh <patelpriyanshnirmalbhai@gmail.com>
2026-01-23 00:22:41 +0530

    Implement valid parentheses checker in Java

---
+++ 020_ValidParentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if(ch == ')' && top != '(') return false;
+                if(ch == ']' && top != '[') return false;
+                if(ch == '}' && top != '{') return false;
+            }
+        } 
+        return stack.isEmpty();
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String : ");
+        String s = sc.nextLine();
+
+        Solution obj = new Solution();
+        System.out.println(obj.isValid(s));
+    }   
+}
